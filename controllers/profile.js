4a73ae35d0bf7b555b8ca780985756b57efb1b98
LawChristopherHau <Lawrencechristopherhau@gmail.com>
2021-09-05 17:34:14 -0400

    readding controllers/profile.js, package-lock.json and package.json

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,54 +1,101 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const User = require("../models/profile")
+const Profile = require('../models/profile-model')
 
-router.get("/", (req,res,next) => {
-    User.find({})
-    .then((profiles) => res.json(profiles))
-    .catch(next);
+router.get('/',(req, res) => {
+    Profile.find({})
+    .then(profiles => {
+        const profilesAug = profiles.map(item => {
+            return{...item.doc, titleEncoded:item.title.split('').join('+')}
+        })
+        res.render('profiles/index', {profiles})
+    })
+    .catch(console.error)
 })
 
-// router.get('/', (req, res) => {
-
-//     User.find({})
-//     .then(users => {
-//         const Aug = profiles.map(item => {
-//             return {...item.doc, titleEncoded:item.title.split('').join('+') }
-//         })
-//         res.render('layout/home', {layout})
-//     })
-//     .catch(console.error)
-// })
-
-router.get("/:id", (req, res, next) => {
-    User.findById(req.params.id)
-    .then((bookmark) => res.json(bookmark))
-    .catch(next);
+router.get('/new', (req, res) => {
+    res.render('profiles/new')
 })
 
 router.post('/', (req, res) => {
-    User.create(req.body).then(profile => {
-        User.find({}).then(profiles => {
-            res.json(profiles)
-        })
+    Profile.create(req.body)
+    .then((profile) => {
+        res.redirect('/profiles');
+    })
+    .catch(console.error);
+})
+
+router.get('/:id/edit', (req, res) => {
+    const id = req.params.id
+    Profile.findById(id)
+    .then (profile => {
+        res.render('profiles/edit', profile)
+    })
+    .catch(console.error)
+})
+
+router.put('/:id', (req,res) => {
+    const id = req.params.id
+    Profile.findOneAndUpdate(
+        { _id: id },
+        {
+        
+        email:                          req.body.email,
+        firstName:                      req.body.firstName,
+        middleName:                     req.body.middleName,
+        lastName:                       req.body.lastName,
+        title:                          req.body.title,
+        location:                       req.body.location,
+        urlPic:                         req.body.urlPic,
+
+        Project1Name:                   req.body.project1Name,
+        Project1Description:            req.body.Project1Description,
+        Project1URL:                    req.body.Project1URL,
+        Project1Img:                    req.body.Project1Img,
+        
+        Project2Name:                   req.body.project2Name,
+        Project2Description:            req.body.Project2Description,
+        Project2URL:                    req.body.Project2URL,
+        Project2Img:                    req.body.Project2Img,
+        
+        Project3Name:                   req.body.project3Name,
+        Project3Description:            req.body.Project3Description,
+        Project3URL:                    req.body.Project3URL,
+        Project3Img:                    req.body.Project3Img,
+        
+        Project4Name:                   req.body.project4Name,
+        Project4Description:            req.body.Project4Description,
+        Project4URL:                    req.body.Project4URL,
+        Project4Img:                    req.body.Project4Img,
+        
+        },
+        { new: true }
+    )
+    .then((profile) => {
+        res.render('profiles/show', profile)
+    })
+});
+
+router.get('/:id', (req, res) => {
+    Profile.findById(req.params.id)
+    .then( profile => {
+        res.render('profiles/show', profile)
+    })
+    .catch(fatalError =>{
+        console.error(fatalError)
+        res.send("There has been a fatal error!")
     })
 })
 
-router.put('/:id', (req, res) => {
-    User.findOneAndUpdate({_id: req.params.id}, req.body)
-    .then(profile => User.find({})
-    .then(profiles => {res.json(profiles)}
-    )
-    )
-})
-
 router.delete('/:id', (req, res) => {
-    User.findOneAndDelete({_id: req.params.id}, req.body)
-    .then(gif => User.find({})
-    .then(profiles => {res.json(profiles)}
-    )
-    )
+    const id = req.params.id;
+    Profile.findOneAndRemove({ _id: id })
+    .then(() => {
+        res.redirect('/profiles');
+    })
+    .catch(console.error)
 })
 
+//creates route
 module.exports = router
